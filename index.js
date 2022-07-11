8d657c0ffb479ed18161c8cba8ef018681c306c9
antoniokallo <antoniokallo@gmail.com>
2022-07-10 23:40:40 -0400

    commit tree 0212c50103637945338c740d9752a661b2af7684

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a+b
+}
+function subtract(a, b) {
+    return a-b
+}
+function multiply(a, b) {
+    return a*b
+}
+function divide (a, b) {
+return a/b
+
+}
+
+var n = 0
+function increment(n) {
+    return (++n)
+}
+function decrement(n){
+    return (--n)
+}
+function makeInt(n){
+
+ const parsed= parseInt(n, 10);
+
+    return parsed * 1
+
+}
+
+function preserveDecimal(n) {
+    const parsed = parseFloat (n)
+    return parsed ;
+    
+  }
